d5f2a0d5579f21d82dcbfd96c695ac8eda01072d
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-05-03 18:50:45 +0200

    u
    u

---
+++ Ficheros/src/Ejercicio1_Clase/Entrada.java
@@ -0,0 +1,19 @@
+package Ejercicio1_Clase;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Entrada {
+
+    Scanner scanner=new Scanner(System.in);
+
+
+    public static  void main(String[] args) {
+
+        ArrayList<DatosBasicos>listaUsuarios;
+
+        listaUsuarios=new ArrayList<>();
+
+
+    }
+}
