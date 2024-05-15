c970236569e0956f04e79b51228f4022f83732d6
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-05-15 21:10:33 +0200

    update

--- Ficheros/src/Simulacro_Ficheros/CrearFichero.java
+++ Ficheros/src/Simulacro_Ficheros/CrearFichero.java
@@ -8,14 +8,28 @@ public class CrearFichero {
 
     public static void main(String[] args) {
 
-        String rutaArchivo="Ficheros/src/resources/";
+        String rutaArchivo="Ficheros/src/resources/ejercicio.txt";
 
 
         try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
             int caracter;
-            while ((caracter = br.read()) != -1) {
+            String linea;
+            while((caracter = br.read()) != -1) {
                 System.out.println("Carácter: " + (char) caracter + ", Código ASCII: " + caracter);
+
+
+            //((caracter = br.read()) != -1) {
+               // System.out.print((char) caracter); la frase sin asci
+
+
+            //((linea = br.readLine()) != null) {
+                //System.out.println(linea); la frase completa
             }
+
+
+            //((caracter = br.read()) != -1) {
+                //System.out.println("Carácter: " + (char) caracter + ", Código ASCII: " + caracter);
+
         } catch (IOException e) {
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
